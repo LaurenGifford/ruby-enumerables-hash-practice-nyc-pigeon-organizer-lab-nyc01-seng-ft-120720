161c76454f0cd03c8c375b4f4e58bc775976ab19
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 05:43:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,8 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
         final_array[name] = {}
         end
-      if !final_array[key] = {}
+      if !final_array[name][key] = {}
+        
       end
     end
   end
@@ -20,18 +21,3 @@ def nyc_pigeon_organizer(data)
 end
 
 
-
-
-
-# result
-# master_key => {key1 => [], key2 => [], key3 => []}
-# repeat for each name
-
-# pigeon_list = {
-# "Theo" = {:color => [pigeon_data[color][purple][0], pigeon_data[color][grey][0]], :gender => pigeon_data[gender][male][1], :lives => pigeon_data["Subway"][0]}
-# "PeterJr." => {},
-# "Lucky" => {},
-# "Ms. K" => {},
-# "Queenie" => {},
-# "Andrew" => {},
-# "Alex" => {}
