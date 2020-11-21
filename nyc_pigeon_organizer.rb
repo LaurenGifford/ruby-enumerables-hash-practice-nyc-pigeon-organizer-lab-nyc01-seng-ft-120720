9f981a11c187622432627729d4af80a45467208c
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 05:45:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,6 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names_array|
       names_array.each do |name|
         binding.pry
-        
         if !final_array[name]
         final_array[name] = {}
         end
