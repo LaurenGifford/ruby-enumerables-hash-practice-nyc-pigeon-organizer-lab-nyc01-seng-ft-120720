66871604a07629abec55f6b239ccdd86a1a4c29c
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 05:32:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do
     |(key, value), final_array|
-    
     value.each do |inner_key, names_array|
-    binding.pry
       names_array.each do |name|
+        binding.pry
         final_array[name] = value
       end
     end
