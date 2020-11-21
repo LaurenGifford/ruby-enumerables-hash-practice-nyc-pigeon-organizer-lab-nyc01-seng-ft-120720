ae21d37f95317ee67a682b82ece6869e563cd76d
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 05:57:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,3 @@
-# pigeon_data = {  :color => {:purple => ["Theo", "Peter Jr.", "Lucky"], :grey => ["Theo", "Peter Jr.", "Ms. K"], :white => ["Queenie", "Andrew", "Ms. K", "Alex"], :brown => ["Queenie", "Alex"]},  :gender => { :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"], :female => ["Queenie", "Ms. K"]}, :lives => { "Subway" => ["Theo", "Queenie"], "Central Park" => ["Alex", "Ms. K", "Lucky"], "Library" => ["Peter Jr."], "City Hall" => ["Andrew"]}}
-
-require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do
@@ -17,7 +14,6 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  binding.pry
 end
 
 
