0c23fbbf73c737646e60e5b92a39eb6fd4e773b0
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 05:49:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do
+  final_results = data.each_with_object({}) do
     |(key, value), final_array|
     value.each do |inner_key, names_array|
       names_array.each do |name|
